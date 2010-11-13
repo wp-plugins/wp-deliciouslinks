ec0945c39792f027e46f98c87cf9f146dc803a8c
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-13 16:54:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-deliciouslinks/trunk@311121 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-deliciouslinks.php
+++ wp-deliciouslinks.php
@@ -1,26 +1,15 @@
-<?php
-/**
- * Main plugin definition
- * Plugin Name: DeliciousLinkSync
- * Plugin URI: 
- * Description: Easily synchronize the links list on your blog or multiple blogs with the links in your delicious account. 
- * Author: DCoda
- * Author URI: http://www.dcoda.co.uk
- * Version: 1.1.15.d6v
- * @package LinkSync
- * @subpackage Plugin
- * @copyright DCoda Ltd
- * @author DCoda Ltd
- * @license http://www.gnu.org/licenses/gpl.txt
- * $HeadURL$
- * $LastChangedDate$
- * $LastChangedRevision$
- * $LastChangedBy$
+<?php /*
+Plugin Name: DeliciousLinkSync
+Plugin URI: http://wordpress.org/extend/plugins/wp-deliciouslinks/
+Description: Easily synchronize the links list on your blog or multiple blogs with the links in your delicious account.
+Author: dcoda
+Author URI: http://dcoda.co.uk
+Version: 4.0.0
  */
 $lib = dirname ( __FILE__ ) . '/library/wordpress/w3v/Application.php';
 if (! file_exists ( $lib )) {
 	require_once dirname ( __FILE__ ) . '/' . basename ( __FILE__, '.php' ) . '/' . basename ( __FILE__ );
 } else {
 	require_once $lib;
-	new w3v_Application ( __FILE__,array() );
+	new w3v_Application ( __FILE__,array('DLinkSyncData','b3v_Http','f1v_Delicious') );
 }
