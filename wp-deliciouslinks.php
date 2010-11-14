d1911fd7d208224164b8d7de1501ad153512eb2d
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 10:57:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-deliciouslinks/trunk@311243 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-deliciouslinks.php
+++ wp-deliciouslinks.php
@@ -4,7 +4,7 @@ Plugin URI: http://wordpress.org/extend/plugins/wp-deliciouslinks/
 Description: Easily synchronize the links list on your blog or multiple blogs with the links in your delicious account.
 Author: dcoda
 Author URI: http://dcoda.co.uk
-Version: 4.0.0
+Version: 4.0.1
  */ 
 $lib = dirname ( __FILE__ ) . '/library/wordpress/w3v/Application.php';
 if (! file_exists ( $lib )) {
