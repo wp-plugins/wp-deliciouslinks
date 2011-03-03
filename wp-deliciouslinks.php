53a66c2eae3794be6000d956b65f77b022e4ee54
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-03 21:12:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-deliciouslinks/trunk@354945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-deliciouslinks.php
+++ wp-deliciouslinks.php
@@ -1,12 +1,13 @@
 <?php /*
-Plugin Name: DeliciousLinkSync
+Plugin Name: &raquo;&raquo;&raquo;&nbsp;DeliciousLinkSync&nbsp;&alpha;&nbsp;&laquo;&laquo;&laquo;
 Plugin URI: http://wordpress.org/extend/plugins/wp-deliciouslinks/
-Description: Easily synchronize the links list on your blog or multiple blogs with the links in your delicious account.
+Description: Easily synchronize the links list on your blog or multiple
+			blogs with the links in your delicious account.
 Author: dcoda
 Author URI: http://dcoda.co.uk
 Version: 4.1.0&alpha;
- */
-require_once dirname ( __FILE__ ) . '/library/wordpress/wv15v/application.php';
+ */ 
+require_once  dirname ( __FILE__ ) . '/library/wordpress/wv15v/application.php';
 @include_once (ABSPATH . '/wp-admin/includes/bookmark.php');
-
-new wv15v_application ( __FILE__, array ('slinksyncdata', 'av15v_delicious', 'bv15v_http' ) );
+	
+new wv15v_application ( __FILE__,array('dlinksyncdata','av15v_delicious','bv15v_http') );
