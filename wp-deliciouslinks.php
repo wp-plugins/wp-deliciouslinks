b686b41601211b8f90aa2328b3beda50dc62cc87
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-04 11:43:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-deliciouslinks/trunk@355181 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-deliciouslinks.php
+++ wp-deliciouslinks.php
@@ -7,7 +7,7 @@ Author: dcoda
 Author URI: http://dcoda.co.uk
 Version: 4.1.0&alpha;
  */ 
-require_once  dirname ( __FILE__ ) . '/library/wordpress/wv15v/application.php';
+require_once  dirname ( __FILE__ ) . '/library/wordpress/application.php';
 @include_once (ABSPATH . '/wp-admin/includes/bookmark.php');
 	
 new wv15v_application ( __FILE__,array('dlinksyncdata','av15v_delicious','bv15v_http') );
