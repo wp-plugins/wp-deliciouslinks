2f383d8e7f36b4534cfd5dccc2fd3a1a679c058e
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-01 20:03:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-deliciouslinks/trunk@469977 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- application/controllers/dlinksyncactionscontroller.php
+++
@@ -1,7 +0,0 @@
-<?php
-class dlinksyncactionscontroller extends wv25v_controller_action_action {
-	public function shutdownAction ()
-	{
-		$this->settings()->update();
-	}
-}
--- application/controllers/dlinksyncfilterscontroller.php
+++
@@ -1,4 +0,0 @@
-<?php
-class dlinksyncfilterscontroller extends wv25v_controller_action_filter {
-}
-		
